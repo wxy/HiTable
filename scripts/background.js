ba58061f06f158e1f32f34245617a9d6db7b4277
Xingyu Wang <xingyu.wang@gmail.com>
2024-04-12 15:26:16 +0800

    完成了点击扩展图标切换的功能
    这里遇到了一个问题，点击扩展的图标不能正确更换。经过研究发现原因如下：
    - 用来切换的图标大小是 512，这是不行的，换成和默认图标一样的 128 的即可
    - 在 scripts/background.js 中指定图标时要以绝对路径或相对该文件的路径来指定，而非以项目根目录为基准。
    
    之前还发现了一个问题是不支持 SVG 图标。

--- scripts/background.js
+++ scripts/background.js
@@ -1,1 +1,19 @@
 console.log("浏览器插件已经启动，并成功加载背景脚本！");
+
+let isActive = false;
+
+chrome.action.onClicked.addListener((tab) => {
+  isActive = !isActive;
+  if (isActive) {
+    // 激活扩展并修改图标
+    chrome.action.setIcon({path: "../src/assets/active.png", tabId: tab.id});
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['scripts/content.js']
+    });
+  } else {
+    // 取消扩展激活并修改图标
+    chrome.action.setIcon({path: "../src/assets/logo.png", tabId: tab.id});
+    // 可能需要编写一些代码来清除或逆转contentScript.js产生的效果
+  }
+});
---
+++ scripts/content.js
@@ -0,0 +1,1 @@
+console.log("content.js 脚本已经成功注入到当前页面！");
